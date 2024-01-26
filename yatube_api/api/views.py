eb1c993639a99c0ba201ff431118bc63dd32d35b
anushST <anushervon.s.06@gmail.com>
2024-01-27 04:47:10 +0500

    Version 1.0

---
+++ yatube_api/api/views.py
@@ -0,0 +1,44 @@
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+
+from posts.models import Group, Post
+
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class UpdateDeletePermissionMixin(viewsets.ModelViewSet):
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied("Удаление чужого контента запрещено!")
+        super().perform_destroy(instance)
+
+
+class PostViewSet(UpdateDeletePermissionMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(UpdateDeletePermissionMixin):
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return post.comments.all()
